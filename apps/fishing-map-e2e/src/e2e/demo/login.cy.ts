c73a7bdad653fe8935b269f82dc37a41b90a9945
FernandoQuijada96 <FernandoQuijada96@hotmail.es>
2025-09-02 17:18:46 +0100

    created demo tests

---
+++ apps/fishing-map-e2e/src/e2e/demo/login.cy.ts
@@ -0,0 +1,25 @@
+import { timeout } from 'es-toolkit'
+
+import { disablePopups, login,switchLanguage } from '../../support/app.po'
+
+describe('vms', () => {
+   before(() => {
+    cy.clearLocalStorage()
+    cy.clearCookies()
+    cy.visit('/')
+    switchLanguage('en')
+    disablePopups()
+    login()
+  })
+
+  it('logins', () => {
+ 
+    cy.getBySel('activity-layer-panel-presence', {timeout: 10000}).should('be.visible').and('have.class', 'print-hidden')
+    cy.getBySel('activity-layer-panel-presence')
+    .find('.LayerPanel-module__V4tG3a__header')
+    .find('button[role="switch"]')
+    .should('be.visible').and('be.enabled').click()
+    cy.getBySel('activity-layer-panel-presence').should('be.visible').and('not.have.class', 'print-hidden')
+
+  })
+})
